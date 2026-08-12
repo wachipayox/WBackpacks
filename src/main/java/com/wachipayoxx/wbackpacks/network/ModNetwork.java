8c8b9f5244f0238de8311bdb17573b0bd7085d1d
Wachii <dani.alvarado.sosa@gmail.com>
2026-08-12 11:25:44 +0200

    feat: register menu quick move payload

--- src/main/java/com/wachipayoxx/wbackpacks/network/ModNetwork.java
+++ src/main/java/com/wachipayoxx/wbackpacks/network/ModNetwork.java
@@ -5,10 +5,11 @@ import net.neoforged.neoforge.network.registration.PayloadRegistrar;
 
 public final class ModNetwork {
     public static void register(RegisterPayloadHandlersEvent event) {
-        PayloadRegistrar registrar = event.registrar("1");
+        PayloadRegistrar registrar = event.registrar("2");
         registrar.playToServer(RequestOpenBackpackPayload.TYPE, RequestOpenBackpackPayload.STREAM_CODEC, RequestOpenBackpackPayload::handle);
         registrar.playToClient(OpenBackpackWindowPayload.TYPE, OpenBackpackWindowPayload.STREAM_CODEC, OpenBackpackWindowPayload::handle);
         registrar.playToServer(BackpackSlotClickPayload.TYPE, BackpackSlotClickPayload.STREAM_CODEC, BackpackSlotClickPayload::handle);
+        registrar.playToServer(MenuSlotQuickMovePayload.TYPE, MenuSlotQuickMovePayload.STREAM_CODEC, MenuSlotQuickMovePayload::handle);
     }
 
     private ModNetwork() {
